fd55f5d978389415887bd362f48769a7a6396f2a
Anoop Kumar <63010032+mr-clover001@users.noreply.github.com>
2024-12-27 16:42:07 +0530

    Create .eslintrc.js

---
+++ src/api/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+        "jsx-a11y/alt-text": "off",
+        "no-unused-vars": "off",
+        "eqeqeq": "off",
+        "react-hooks/exhaustive-deps": "warn" // or "error" if you want strict checking
+    }
+};
